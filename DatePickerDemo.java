4b75c6cb062f051342e20fb65d1df394769a13dd
manojmoola <mulamanoj25@gmail.com>
2025-09-18 23:20:12 +0530

    Update DatePickerDemo.java

--- DatePickerDemo.java
+++ DatePickerDemo.java
@@ -13,23 +13,23 @@ public class DatePickerDemo {
         driver.manage().window().maximize();
         driver.get("https://jqueryui.com/datepicker/");
 
-        // STEP 1: Switch to the iframe first
+        
         WebElement iframe = driver.findElement(By.className("demo-frame"));
         driver.switchTo().frame(iframe);
 
-        // STEP 2: Open the date picker
+        
         WebElement dateField = driver.findElement(By.id("datepicker"));
         dateField.click();
 
         Thread.sleep(1000); // small wait for calendar to be visible
 
-        // STEP 3: Go to next month
+        
         WebElement nextButton = driver.findElement(By.xpath("//a[@title='Next']"));
         nextButton.click();
 
-        Thread.sleep(1000); // wait for next month to load
+        Thread.sleep(1000); 
 
-        // STEP 4: Select date "22"
+        
         List<WebElement> allDates = driver.findElements(By.xpath("//a[@class='ui-state-default']"));
         for (WebElement date : allDates) {
             if (date.getText().equals("22")) {
@@ -38,7 +38,7 @@ public class DatePickerDemo {
             }
         }
 
-        // STEP 5: Print selected date
+        
         System.out.println("Selected Date: " + dateField.getAttribute("value"));
 
         driver.quit();
